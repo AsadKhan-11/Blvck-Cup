89336f76f3436c56073ae1344184e5f17a429157
Asad Khan <mrasad10khan@gmail.com>
2025-08-04 21:35:43 +0500

    Minor Adjustments

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -21,9 +21,9 @@ const Hero = () => {
           <Navbar sidebar={sidebar} setSidebar={setSidebar} />
           {/* Hero section */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 min-h-[850px] place-items-center">
-            <div className="lightOrange mt-[100px] p-4 md:mt-0 space-y-24">
+            <div className="max-w-[100%] lightOrange mt-[100px] p-4 md:mt-0 space-y-24">
               <motion.h1
-                className="text-7xl font-bold ml-14 leading-tight "
+                className="text-6xl font-bold  leading-tight text-center md:text-left "
                 initial={{ opacity: 0, y: -100 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{
@@ -33,12 +33,12 @@ const Hero = () => {
                   stiffness: 100,
                 }}
               >
-                Blvck Cup
+                SIP IN BLACK ELEGANCE{" "}
               </motion.h1>
-              <div className="relative ">
-                <div className=" relative z-10 space-y-4">
+              <div className="relative  ">
+                <div className=" relative z-10 space-y-4 ml-2 max-w-[350px] ">
                   <h2 className="text-2xl">Black Lifestyle Lovers</h2>
-                  <p className="opacity-55 leading-loose text-sm">
+                  <p className="opacity-55 leading-loose text-sm ">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Voluptatem quo laborum expedita nisi cumque? Et, quos. Iste
                     doloribus optio ut, voluptatum vel explicabo exercitationem
@@ -64,25 +64,17 @@ const Hero = () => {
               />
               {/* orange circle ring */}
               <motion.div
-                className="absolute rounded-full top-24 -right-16 primary border-[20px] h-[180px] w-[180px] z-10 "
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
+                className="absolute rounded-full top-24  -right-16 primary border-[20px] h-[180px] w-[180px] z-10 "
+                initial={{ clipPath: "circle(0% at 50% 50%)" }}
+                animate={{ clipPath: "circle(75% at 50% 50%)" }}
                 transition={{
                   delay: 0.4,
-                  type: "spring",
-                  damping: 10,
-                  stiffness: 100,
+                  duration: 2,
                 }}
               ></motion.div>
-
-              <div className="absolute -top-20 left-[200px] z-[1]">
-                <div className="darkGrey opacity-40 leading-none text-[140px] scale-120 font-bold ml-4">
-                  Blvck Cup
-                </div>
-              </div>
             </div>
             {/* Third Section */}
-            <div className="lightOrange mt-[100px] p-4 md:mt-0 space-y-24">
+            <div className="relative lightOrange mt-[100px] p-4 md:mt-0 space-y-24 hidden lg:block">
               <h1 className="opacity-0 text-7xl font-bold ml-14 leading-tight ">
                 Blvck Cup
               </h1>
@@ -95,6 +87,11 @@ const Hero = () => {
                   </p>
                 </div>
                 <div className="absolute -top-6 right-0 w-[250px] h-[180px] bg-gray-950 opacity-50 "></div>
+              </div>
+              <div className="absolute -top-30 left-[0] z-[1] ">
+                <div className="darkGrey opacity-5 leading-none text-[120px]  scale-120 font-bold ml-4">
+                  Blvck Cup
+                </div>
               </div>
             </div>
           </div>
